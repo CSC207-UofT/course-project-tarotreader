d5debf03d535a9e55ca6fea7e12270365a51565d
Elif Erzincan <elif.erzincan@mail.utoronto.ca>
2021-10-16 16:44:57 -0400

    changed reading log attribute of user

--- src/main/java/entities/user.java
+++ src/main/java/entities/user.java
@@ -1,31 +1,26 @@
 package entities;
 
-import java.util.ArrayList;
 
 public class user {
     String username;
     String birthday;
     readingLog readingLog;
-    public user(String username, int year, int month, int day, readingLog r){
+
+    public user(String username, int year, int month, int day, readingLog r) {
         this.username = username;
         this.birthday = year + "/" + month + "/" + day;
         this.readingLog = r;
     }
 
-    public String getUsername(){
+    public String getUsername() {
         return this.username;
     }
 
-    public String getBirthday(){
+    public String getBirthday() {
         return this.birthday;
     }
 
-    public ArrayList getReadingLog(){
-        return this.readingLog;
-    }
+    public readingLog getReadingLog() {
+        return this.readingLog; }
 
-    public static void main(String[] args){
-        user elif = new user("eliferzincan", 2001, 4, 30);
-        System.out.println(elif.getBirthday());
-    }
 }
