3785b539bc1a60136657bd0ec6d2816cd2c321ee
bahati365 <damien.bahati@mail.utoronto.ca>
2021-10-16 18:06:48 -0400

    Coming along with commands

--- src/main/java/commands/commandExecutor.java
+++ src/main/java/commands/commandExecutor.java
@@ -5,7 +5,7 @@ public class commandExecutor {
      *Executes the provided commandLine
      * @param shellState the state of the program
      * @param commandLine is the given command line
-     * @return string that is given back when command is executed
+     * @return String that is given back when command is executed
      * @throws Exeption if any of the provided commands/arguments is invalid
      */
     public String executeCommand(IShellState shellState string commandLine)
