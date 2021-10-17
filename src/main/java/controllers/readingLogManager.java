b73b1b8efaf2ab867e401ecf72ed23457a42aaeb
murtiturti <murat.diken@mail.utoronto.ca>
2021-10-17 15:54:00 -0400

    Changes in SpreadManager and ReadingLog, documentation and CRC models added - Bora and Murat

--- src/main/java/controllers/readingLogManager.java
+++ src/main/java/controllers/readingLogManager.java
@@ -4,15 +4,17 @@ import entities.user;
 import useCases.tarotReader;
 
 import java.util.HashMap;
-import java.util.List;
-import java.util.ArrayList;
 import java.util.Map;
 
 
 public class readingLogManager {
     private static Map<user, readingLog> logs = new HashMap<user, readingLog>();
 
-    // If the user does not have a reading log, returns null.
+    /**
+     * Returns the log of the selected user
+     * @param user Selected user
+     * @return ReadingLog object of the given user. If the user does not have a reading log, returns null.
+     */
     public static readingLog viewLog(user user) {
         readingLog log = null;
         if (logs.containsKey(user)) {
@@ -21,7 +23,12 @@ public class readingLogManager {
         return log;
     }
 
-    // Returns true if the reading is successfully added to the log, false otherwise.
+    /**
+     * Adds the reading to the log of the specified user.
+     * @param user The specified user
+     * @param reading A tarot reading
+     * @return Returns true if the reading is successfully added to the log, false otherwise.
+     */
     public static boolean logReading(user user, tarotReader reading) {
         if (logs.containsKey(user)) {
             logs.get(user).addToLog(reading);
@@ -30,6 +37,10 @@ public class readingLogManager {
         return false;
     }
 
+    /**
+     * Adds the user and the user's reading log to the logs map.
+     * @param user The specified user object.
+     */
     public static void addLog(user user) {
         logs.put(user, user.getReadingLog());
     }
--- src/main/java/entities/readingLog.java
+++ src/main/java/entities/readingLog.java
@@ -5,21 +5,34 @@ import java.util.ArrayList;
 
 public class readingLog {
     private List<tarotReader> readings;
-    private user belongingUser;
 
-    public readingLog(user user) {
-        this.belongingUser = user;
+    /**
+     * Constructs a new reading log
+     */
+    public readingLog() {
         this.readings = new ArrayList<tarotReader>();
     }
 
+    /**
+     * Getter method for the readings
+     * @return Returns list of readings for this user log.
+     */
     public List<tarotReader> getReadings() {
         return this.readings;
     }
 
+    /**
+     * Adds the reading to this log
+     * @param reading the tarot reading to add
+     */
     public void addToLog(tarotReader reading) {
         readings.add(reading);
     }
 
+    /**
+     * Override toString method
+     * @return A string representation of all the readings in the reading log.
+     */
     @Override
     public String toString() {
         StringBuilder logs = new StringBuilder();
