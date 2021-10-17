04995ac97638ed3e6432e6f9791066135119c634
bahati365 <damien.bahati@mail.utoronto.ca>
2021-10-17 02:38:45 -0400

    Coming along with commands

--- src/main/java/commands/commandExecutor.java
+++ src/main/java/commands/commandExecutor.java
@@ -8,6 +8,7 @@ public class commandExecutor {
      * @return string that is given back when command is executed
      * @throws Exeption if any of the provided commands/arguments is invalid
      */
+
     public String executeCommand(IShellState shellState string commandLine)
             throws Exeption{
         shellState.addHistory(commandLine);
