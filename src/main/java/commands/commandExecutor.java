a38c48c810080422ead3a42de98e20c74856e1b0
bahati365 <damien.bahati@mail.utoronto.ca>
2021-10-17 14:04:41 -0400

    Coming along with commands

--- src/main/java/commands/commandExecutor.java
+++ src/main/java/commands/commandExecutor.java
@@ -3,18 +3,14 @@ package commands;
 public class commandExecutor {
     /**
      *Executes the provided commandLine
-     * @param shellState the state of the program
      * @param commandLine is the given command line
      * @return String that is given back when command is executed
      * @throws Exeption if any of the provided commands/arguments is invalid
      */
-    public String executeCommand(IShellState shellState string commandLine)
+    public String executeCommand(commandLine)
             throws Exeption{
-        shellState.addHistory(commandLine);
-        if (commandLine.equals("")){
-            return "";
-        }
-
+        commandLine.equals("");
+        return "";
     }
 
 }
