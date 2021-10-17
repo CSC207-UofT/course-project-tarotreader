53c58b0e880abac12921835fe449783e71949632
Elif Erzincan <elif.erzincan@mail.utoronto.ca>
2021-10-17 16:37:57 -0400

    I deleted ==== in shuffler and implemented the class shuffleManager

--- src/main/java/controllers/shuffleManager.java
+++ src/main/java/controllers/shuffleManager.java
@@ -1,11 +1,22 @@
 package controllers;
-
-import entities.deck;
 import useCases.shuffler;
+import entities.card;
+import java.util.ArrayList;
 
 public class shuffleManager extends shuffler{
+    public static ArrayList<card> shuffleNTimes(int n) throws Exception {
+        ArrayList<card> result = new ArrayList<card>();
+        if (n <= 0 || n > 10){
+            throw new Exception("Please enter a valid input between 1 and 10");
+        }
+        for (int i = 1; i == n; i++){
+            result = shuffleDeck();
+        }
+        return result;
+    }
 
-    public void getCopy(deck d){
-        //String[] clonedDeck = n;
+    public static void main(String[] args) throws Exception {
+        System.out.println(shuffleNTimes(4));
+        System.out.println(shuffleNTimes(-3));
     }
 }
--- src/main/java/useCases/shuffler.java
+++ src/main/java/useCases/shuffler.java
@@ -9,8 +9,7 @@ import java.util.Collections;
 public class shuffler {
     public static ArrayList<card> shuffleDeck(){
         ArrayList<card> deckClone = (ArrayList<card>)deck.getDeck().clone();
-=======
-  
+
         Random random = new Random();
         int randomNumber = random.nextInt(10) + 1;
         for (int i = 0; i < randomNumber; i++){
@@ -19,7 +18,7 @@ public class shuffler {
         return deckClone;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println(shuffleDeck());
     }
 }
