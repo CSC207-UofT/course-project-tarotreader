712dfa7af804f4c39975c021ccdb642cdbc83545
Shaun <46703168+shaunamia@users.noreply.github.com>
2021-10-17 22:05:07 -0400

    Update cardPicker.java

--- src/main/java/controllers/cardPicker.java
+++ src/main/java/controllers/cardPicker.java
@@ -1,22 +1,17 @@
 package controllers;
 
-import entities.card;
-import useCases.shuffler;
-
-import java.io.IOException;
+import entities.Card;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class cardPicker {
-
-    private static final ArrayList<card> shuffledDeck = shuffler.shuffleDeck();
-    private static ArrayList<card> pickedCards = new ArrayList<>();
+public class CardPicker {
+    private static ArrayList<Card> pickedCards = new ArrayList<>();
     private static final Random rand = new Random();
     private static final int index1 = rand.nextInt(78);
     private static final int index2 = rand.nextInt(78);
     private static final int index3 = rand.nextInt(78);
 
-    public static ArrayList<card> getPickedCards(){
+    public static ArrayList<Card> getPickedCards(ArrayList<Card> shuffledDeck){
         pickedCards.add(shuffledDeck.get(index1));
         pickedCards.add(shuffledDeck.get(index2));
         pickedCards.add(shuffledDeck.get(index3));
@@ -24,3 +19,4 @@ public class cardPicker {
     }
 
 }
+
