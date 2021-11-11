a596f74ac38c639a2d4d911e0554d6dad35ea677
shaunamia <shauna.vronces@mail.utoronto.ca>
2021-11-10 22:19:32 -0500

    Implemented DataReader.java -- it is no longer an interface, it has readData method. SpreadInit and CardInit now use DataReader.readData with the appropriate constants file as an input to initialize their respective objects and return a list of their initialized objects

--- src/main/java/controllers/DataReader.java
+++ src/main/java/controllers/DataReader.java
@@ -1,4 +1,32 @@
 package controllers;
 
-public class DataReader <Interface>{
+import entities.Card;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataReader {
+
+    public static ArrayList<String[]> readData(String fileName) throws IOException {
+        ArrayList<String[]> data = new ArrayList<>();
+        BufferedReader dataReader = null;
+        try {
+            dataReader = new BufferedReader(new FileReader(fileName));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        String line;
+        while (true) {
+            assert false;
+
+            if ((line = dataReader.readLine()) == null) break;
+            String[] elem = line.split(",");
+            data.add(elem);
+        }
+        return data;
+    }
 }
