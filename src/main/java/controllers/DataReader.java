9668164fea616b6509e14dbb77bb5c68175c784b
jaimans <jai.mansukhani@mail.utoronto.ca>
2021-11-12 15:52:05 -0500

    New files that have implementations of scanner for the use of reading data from a csv file (cardInit and data reader)

--- src/main/java/controllers/DataReader.java
+++ src/main/java/controllers/DataReader.java
@@ -1,4 +1,22 @@
 package controllers;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class DataReader <Interface>{
+public class DataReader {
+
+    public static ArrayList<String[]> readData(String fileName) throws IOException{
+        ArrayList<String[]> data = new ArrayList<>();
+        Scanner dataReader = null;
+        try{
+            dataReader = new Scanner(new File(fileName));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        dataReader.useDelimiter(",");
+        while (dataReader.hasNext()){
+            System.out.println(dataReader.next());
+        }
+        return data;
+    }
 }
--- src/main/java/useCases/CardInit.java
+++ src/main/java/useCases/CardInit.java
@@ -1,42 +1,39 @@
 package useCases;
 
+import controllers.DataReader;
 import entities.Card;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class CardInit {
     private static ArrayList<Card> cardList = new ArrayList<Card>();
-    private static BufferedReader TarotReader;
-
-    static {
-        try {
-            TarotReader = new BufferedReader(new FileReader("resources/cardConstants"));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public CardInit() throws FileNotFoundException {
-    }
-
 
     public static ArrayList<Card> getCardList() throws IOException {
-        String line;
-        while ((line = TarotReader.readLine()) != null) {
-            String[] elem = line.split(",");
+        ArrayList<String[]> data = DataReader.readData("/Users/user/Downloads/cardconstants.csv");
+        int length = data.size();
+        int i = 0;
+        while (i < length){
+            String[] currline = data.get(i);
             Card newCard = null;
-            if (elem.length == 4) {
-                newCard = new Card(elem[0].strip(), elem[1].strip(), elem[2].strip(), elem[3].strip());
-            } else if (elem.length == 5) {
-                newCard = new Card(elem[0].strip(), elem[1].strip(), elem[2].strip(), elem[3].strip(), elem[4].strip());
+            if (currline.length == 4) {
+                newCard = new Card(currline[0].strip(), currline[1].strip(), currline[2].strip(), currline[3].strip());
+            } else if(currline.length == 5){
+                newCard = new Card(currline[0].strip(), currline[1].strip(), currline[2].strip(), currline[3].strip(), currline[4].strip());
             }
             cardList.add(newCard);
-
+            i ++;
         }
         return cardList;
     }
+    public static void main (String[] args) throws IOException {
+        cardList = getCardList();
+        int counter = 0;
+        for (int i = 0; i < cardList.size(); i++){
+            System.out.println(cardList.get(i).getName());
+            counter++;
+        }
+        System.out.println(counter);
+    }
 }
+
