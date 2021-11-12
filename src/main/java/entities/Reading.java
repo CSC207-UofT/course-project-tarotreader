ba4c52b09dc7725b40b8eeb106a67e544b8026f7
yigitelmas <yigitalpelmas@gmail.com>
2021-11-12 15:21:15 -0500

    readingGenerator and Reading entitty class are updated- but not final

--- src/main/java/entities/Reading.java
+++ src/main/java/entities/Reading.java
@@ -1,4 +1,36 @@
 package entities;
 
+import entities.Card;
+import entities.Deck;
+import entities.User;
+import java.util.ArrayList;
+
+
 public class Reading {
+    public ArrayList<Card> chosenCards;
+    public ArrayList<String> reading;
+    public String name;
+    public Spread spread;
+    public Reading (ArrayList<Card> pickedCards, User user, Spread spread){
+        this.chosenCards = pickedCards;
+        this.reading = new ArrayList<>(chosenCards.size());
+        this.name = user.username;
+        this.spread = spread;
+        int i = 0;
+        for (Card c: chosenCards){
+            this.reading.set(i, c.getMeaning());
+            i ++;
+        }
+    }
+    @Override
+    public String toString(){
+        StringBuilder myString = new StringBuilder();
+        String s = "Hey " + this.name + "! Here is your reading for today:\n";
+        for (String r: this.reading){
+            myString.append(r);
+            myString.append("\n");
+        }
+        s += myString;
+        return s;
+    }
 }
