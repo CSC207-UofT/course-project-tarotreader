9e60d75eca54f677305c21f659b2bee45d971be3
bahati365 <damien.bahati@mail.utoronto.ca>
2021-11-14 12:55:43 -0500

    UserGenerator, deck and UserManager

--- src/main/java/CommandLineInterface.java
+++ src/main/java/CommandLineInterface.java
@@ -1,16 +1,46 @@
+import entities.User;
 import useCases.*;
-
-import java.util.Objects;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CommandLineInterface{
     public static void main(String[] args) {
+        ArrayList<User> users = new ArrayList();
+        users.add(new User("bahati", "password", 1999, 7, 9));
+
         System.out.println("Welcome to Tarot Reader 3000");
         Scanner account = new Scanner(System.in);
         System.out.println("Please enter 1 for logging in or 2 for creating a new account");
-        String entry = account.nextLine();
-        //if (Objects.equals(entry, "1")){
-        //    UserGenerator.generate();
-        //}
+        int entry = Integer.parseInt(account.nextLine());
+        if(entry == 1)
+        {
+            // Ask for Username
+            String username = account.nextLine();
+            // Ask for Password
+            String password = account.nextLine();
+            // Do Login
+            try {
+                LogIn.login(username, password, users);
+            } catch(Exception e) {
+                System.out.println("Error: No Such User Found!");
+            }
+        } else if(entry == 2)
+        {
+            // Ask for Username
+            System.out.println("Enter Username: ");
+            String username = account.nextLine();
+            // Ask for Password
+            System.out.println("Enter Password: ");
+            String password = account.nextLine();
+            // Ask for Date of Birth
+            System.out.println("Enter Date of Birth");
+            String dob = account.nextLine();
+            int year = Integer.parseInt(dob.split("-")[0]);
+            int month = Integer.parseInt(dob.split("-")[1]);
+            int day = Integer.parseInt(dob.split("-")[2]);
+            // Generate a User
+            UserGenerator.generateUser(username, password, year, month, day);
+        }
+
     }
 }
--- src/main/java/useCases/UserGenerator.java
+++ src/main/java/useCases/UserGenerator.java
@@ -3,6 +3,11 @@ package useCases;
 import controllers.UserManager;
 import entities.User;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class UserGenerator{
     //creates a new user object based on inputs from user manager
     // date, user name, password and age
@@ -15,6 +20,20 @@ public class UserGenerator{
         password = initPassword;
         // creates a new user object
         UserManager User = new UserManager(initUserName, initBirthday, initPassword);
+    }
+
+    public static void generateUser(String username, String password, int year, int month, int day) {
+        User user = new User(username, password, year, month, day);
+        try {
+            FileOutputStream outFile = new FileOutputStream("/users/" + username + ".ser");
+            ObjectOutputStream out = new ObjectOutputStream(outFile);
+            out.writeObject(user);
+            out.close();
+            outFile.close();
+        }
+        catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
     }
 
     public String getPassword() {
