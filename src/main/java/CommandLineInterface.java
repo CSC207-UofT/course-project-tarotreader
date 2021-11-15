22af80dfa688f599aaa633503d84fe9107f595c0
bahati365 <damien.bahati@mail.utoronto.ca>
2021-11-14 23:24:20 -0500

    CommandlineInterface code done, covering all the use cases and required methods. only thing is that I couldn't read from a file. so thats something which will have to be fixed

--- src/main/java/CommandLineInterface.java
+++ src/main/java/CommandLineInterface.java
@@ -1,8 +1,8 @@
+import entities.Card;
+import entities.Reading;
+import entities.Spread;
 import entities.User;
 import useCases.*;
-
-import java.io.FileInputStream;
-import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -18,14 +18,53 @@ public class CommandLineInterface{
         if(entry == 1)
         {
             // Ask for Username
+            System.out.println("Enter username:");
             String username = account.nextLine();
             // Ask for Password
+            System.out.println("Enter password:");
             String password = account.nextLine();
             // Do Login
             try {
-                LogIn.login(username, password, users);
+                // Try login
+                User userLoggedIn = LogIn.login(username, password, users);
+                // Call getCardList() to get the card list
+                ArrayList<Card> cardList = CardInit.getCardList();
+                // Ask for number of times to shuffle
+                System.out.println("How many times do you want to shuffle?");
+                int shuffleCount = Integer.parseInt(account.nextLine());
+                // Allow user to select 3 cards
+                ArrayList<Integer> indicesChosen = new ArrayList<>();
+                System.out.println("There are " + cardList.size() + " cards");
+                for(int i = 0;i<3;i++) {
+                    System.out.println("Please select card #" + (i + 1) + ": ");
+                    indicesChosen.add(Integer.parseInt(account.nextLine()));
+                }
+                // Ask for Spread
+                ArrayList<Spread> spreads = SpreadInit.getSpreads();
+                System.out.println("Size of spreads is " + spreads.size());
+
+                System.out.println("What spread do you want?");
+                for(int i = 0;i<spreads.size();i++)
+                {
+                    System.out.println(i + " " + spreads.get(i));
+                }
+                int spreadNumChosen = Integer.parseInt(account.nextLine());
+                String spreadName = spreads.get(spreadNumChosen).getSpreadName();
+                System.out.println("Enter Number of Cards: ");
+                String numOfCards = account.nextLine();
+                Spread spread = new Spread(spreadName, numOfCards);
+                // Generate Reading
+                Reading theReading = ReadingGenerator.generateReading(userLoggedIn, spread, shuffleCount, indicesChosen);
+                // Instantiate a ReadingLogger
+                ReadingLogger readingLogger = new ReadingLogger(userLoggedIn);
+                // Add reading to the ReadingLogger
+                readingLogger.logReading(theReading);
+                // Present the Reading to the User
+                System.out.println(theReading.toString());
+
             } catch(Exception e) {
                 System.out.println("Error: No Such User Found!");
+                System.out.println(e);
             }
         } else if(entry == 2)
         {
