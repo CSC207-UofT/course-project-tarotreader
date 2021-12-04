3465fda7fd71d212774b911c1983b3fba330d2df
Elif Erzincan <elif.erzincan@mail.utoronto.ca>
2021-12-04 16:39:32 -0500

    added a password checking method in UserGenerator and added test cases to UserGeneratorTest.

--- src/main/java/useCases/UserGenerator.java
+++ src/main/java/useCases/UserGenerator.java
@@ -3,6 +3,8 @@ package useCases;
 import entities.User;
 
 import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 //Class for generating users
 
@@ -20,9 +22,12 @@ public class UserGenerator {
         //try to deserialize the User from its filename(username.ser)
         User existingUser = null;
         existingUser = getExistingUser(username);
-        if (existingUser == null){
+        if (existingUser != null){
             System.out.println("Username already exists!");
             }
+        else if (!validPassword(password)) {
+            System.out.println("Please select a password that is valid.");
+        }
         else{
             User user = new User(username, password, year, month, day); //Creates the User object
             //Tries to serialize the object in this try block
@@ -37,7 +42,7 @@ public class UserGenerator {
                 ioException.printStackTrace();
         }}
     }
-    private static User getExistingUser(String username) {
+    public static User getExistingUser(String username) {
         User existingUser = null;
         try {
             FileInputStream inputStream = new FileInputStream(username + ".ser");
@@ -46,9 +51,29 @@ public class UserGenerator {
             in.close();
             inputStream.close();
         }
+        catch (FileNotFoundException ex){
+            return existingUser;
+        }
         catch(ClassNotFoundException | IOException ioException) {
             ioException.printStackTrace();
             }
         return existingUser;
     }
-}
+    public static boolean validPassword(String password){
+        //enforces a password that is shown by the regex shown below.
+        // a "valid" password consists of at least one digit, one lowercase letter,
+        // one uppercase letter, and it needs to be between 8-20 characters without a
+        // whitespace.
+        String regex = "^(?=.*[0-9])"
+                + "(?=.*[a-z])(?=.*[A-Z])"
+                + "(?=\\S+$).{8,20}$";
+        Pattern p = Pattern.compile(regex);
+        if (password == null) {
+            return false;
+        }
+        //return true iff password matches the pattern.
+        Matcher m = p.matcher(password);
+        return m.matches();
+        }
+    }
+
