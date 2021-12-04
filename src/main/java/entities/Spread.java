8db174b580ffde46a037b675a1d12b37af109e57
Shaun <46703168+shaunamia@users.noreply.github.com>
2021-12-04 14:51:44 -0500

    Added a requiredMeaningType attribute to Spread.java class, now throws WrongSpreadType exception if Spread type does not exist. Made relevant changes in SpreadInit.java.

--- src/main/java/entities/Spread.java
+++ src/main/java/entities/Spread.java
@@ -4,18 +4,35 @@ import useCases.SpreadInit;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Spread {
+    private static final String GEN = "General Reading";
+    private static final String PPF = "Past Present Future Reading";
+    private static final String LOVE = "Love Reading";
+    private static final String CAREER = "Career Reading";
     /***
      * Holds constructor and methods for Spread objects
      */
 
     private String spreadName;
     private int numberOfCards;
+    private final String requiredMeaningType;
 
-    public Spread(String spreadName, String numberOfCards){
+    public Spread(String spreadName, String numberOfCards) throws WrongSpreadType{
         this.spreadName = spreadName;
         this.numberOfCards = Integer.parseInt(numberOfCards);
+        if(Objects.equals(spreadName, GEN)){
+            this.requiredMeaningType = "General";
+        }else if(Objects.equals(spreadName, PPF)){
+            this.requiredMeaningType = "General";
+        }else if(Objects.equals(spreadName, LOVE)){
+            this.requiredMeaningType = "Love";
+        }else if(Objects.equals(spreadName, CAREER)){
+            this.requiredMeaningType = "Career";
+        }else{
+            throw new WrongSpreadType("Spread type does not exist. Please try again");
+        }
     }
 
     public String getSpreadName(){
@@ -28,9 +45,12 @@ public class Spread {
         return this.numberOfCards;
     }
 
-    public static ArrayList<Spread> getSpreadList() throws IOException {
+    public static ArrayList<Spread> getSpreadList() throws IOException, WrongSpreadType {
         /* Returns an array list of all available Spread objects */
         return SpreadInit.getSpreads();
+    }
+    public static class WrongSpreadType extends Exception {
+        public WrongSpreadType (String errorMessage) {super(errorMessage);}
     }
 }
 
