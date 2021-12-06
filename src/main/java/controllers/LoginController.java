086e28a79e013bd4c50f0417911a524d449598bc
murtiturti <murat.diken@mail.utoronto.ca>
2021-12-05 23:38:09 -0500

    Moved data deserialization to controller and simplified login use case

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -1,28 +1,67 @@
 package controllers;
 
+import entities.User;
 import useCases.LogIn;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.Objects;
 
 public class LoginController {
 
+    // Instance of the singleton
     private static final LoginController controllerInstance = new LoginController();
-    private LogIn logIn;
 
     private LoginController() {
         // Private constructor so that client-side cannot instantiate this singleton
     }
 
+    /**
+     * Instance access method for singleton design pattern
+     * @return This instance of LoginController
+     */
     public static LoginController getInstance() {
         return controllerInstance;
-    }
+    } // getter for the singleton instance
 
+
+    /**
+     * Method for accessing the user database and deserializing given user
+     * @param username Username of the user to deserialize
+     * @param password Password of the user to deserialize
+     * @return True iff User file exists and password is match, false otherwise.
+     */
     public Object login(String username, String password) {
-        logIn = new LogIn(username, password);
+        User loggedInUser = null;
+        //Try-catch block for deserializing User object with the given username and password
         try {
-            return logIn.login();
+            FileInputStream inputStream = new FileInputStream(username + ".ser"); //Tries to find user file
+            ObjectInputStream in = new ObjectInputStream(inputStream);
+            loggedInUser = (User) in.readObject(); //Casts to user
+            in.close();
+            inputStream.close();
+            //Passes to the LogIn use case for password check
+            LogIn logIn = new LogIn(password, loggedInUser);
+            // If the login method returns true, the user is matched and this controller tells the CLI by returning true
+            if (logIn.login()) {
+                return loggedInUser;
+            }
+            else {
+                return false;
+            }
         }
-        catch (Exception e) {
-            return false;
+        //Exception handling blocks
+        catch(IOException ioException) {
+            ioException.printStackTrace();
         }
+        catch(ClassNotFoundException fileException) {
+            System.out.println("There isn't a user registered with this username");
+        }
+        catch (ClassCastException castException) {
+            System.out.println("");
+        }
+        return false; // Returns false if an exception is caught, because this means the user does not exist.
     }
 
 }
