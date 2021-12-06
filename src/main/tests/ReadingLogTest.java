78e3e8a0abe7f2c42a8cf828fa00b49485a3e7d7
Shaun <46703168+shaunamia@users.noreply.github.com>
2021-12-05 20:19:21 -0500

    Modified Test in reading Log test to take the right inputs, added extra lines that will be filled later for cardConstants so cards initialize properly

--- src/main/tests/ReadingLogTest.java
+++ src/main/tests/ReadingLogTest.java
@@ -1,6 +1,9 @@
 import entities.*;
 import org.junit.Test;
+import useCases.ReadingGenerator;
+
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class ReadingLogTest {
 
@@ -23,9 +26,15 @@ public class ReadingLogTest {
             User u = new User("eliferzincan", "20012001", 2001, 4, 30);
             ReadingLog readingLog = new ReadingLog(u);
             Spread s = new Spread("ExampleSpread", "50");
+            ArrayList<Card> shuffledDeck = ReadingGenerator.shuffleDeck(3);
             ArrayList<Card> a = new ArrayList<>();
-            Reading r = new Reading(u, s);
-            r.addCardMeanings(a);
+            ArrayList<Integer> indecies = new ArrayList<>();
+            indecies.add(1);
+            indecies.add(2);
+            indecies.add(3);
+            a.addAll(ReadingGenerator.pickCard(shuffledDeck, indecies));
+            Reading r = new Reading(a, u, s);
+            r.addCardMeanings(a, s);
             readingLog.addToLog(r);
             assert !readingLog.getReadings().isEmpty();
         } catch (Exception e) {
