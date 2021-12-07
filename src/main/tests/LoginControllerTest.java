a040904eb57d99a59441946f0ddd2dbb9b46f159
bahati365 <damien.bahati@mail.utoronto.ca>
2021-12-07 14:03:23 -0500

    Tests in progress. most likely be done tonight

---
+++ src/main/tests/LoginControllerTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LoginControllerTest {
+
+    @Test
+    public void LoginControllerTest() {
+
+    }
+
+}
---
+++ src/main/tests/ReadingGeneratorControllerTest.java
@@ -0,0 +1,28 @@
+import controllers.ReadingGeneratorController;
+import entities.Card;
+import entities.Reading;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ReadingGeneratorControllerTest {
+
+    @Test
+    public void TestShuffleDeck(){
+
+    }
+
+    @Test
+    public void TestPickCards(){
+
+    }
+
+    @Test
+    public void TestGenerateReading(){
+
+    }
+}
+
